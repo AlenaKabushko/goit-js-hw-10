e43596c4daeb3af17354be646dbad885eb95d66a
Alena <100042777+AlenaKabushko@users.noreply.github.com>
2022-09-05 13:19:35 +0300

    add task with comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
 //import templateOneCountry from './templates/templateOneCountry.hbs';
 //import templateCountries from './templates/templateCountries.hbs'
 
@@ -32,8 +33,11 @@ console.log(refs)
 
 const render = (countryList) => {
     if (countryList.length === 1) {
+        refs.countryListRef.innerHTML = "";
         renderOne(countryList);
+
     } else if (countryList.length > 1 && countryList.length <= 10) {
+        refs.countryInfoRef.innerHTML = "";
         renderAll(countryList);
     }
 }
@@ -41,28 +45,28 @@ const render = (countryList) => {
 const renderOne = (countryList) => {
     const render = countryList.map(
         ({ name, capital, population, flags, languages }) =>
-        `<div>
-            <img src="${flags.svg}" alt="flag" width="25">
-            <p>${name.official}</p>
+            `<div class="country-info__title">
+            <img src="${flags.svg}" alt="flag" width="50">
+            <p class="country-info__name">${name.official}</p>
         </div>
-        <p>Capital: ${capital}</p>
-        <p>Population: ${population}</p>
-        <p>Languages:${languages}</p>`
-    ).join('')
+        <p class="country-info__info"><span class="bold">Capital:</span> ${capital}</p>
+        <p class="country-info__info"><span class="bold">Population:</span> ${population}</p>
+        <p class="country-info__info"><span class="bold">Languages:</span> ${Object.values(languages)}</p>`
+    ).join('');
     
-    refs.countryInfoRef.innerHTML = render
+    refs.countryInfoRef.innerHTML = render;
 }
 
 const renderAll = (countryList) => {
     const render = countryList.map(
         ({ name, flags }) =>
-        `<li>
+            `<li class="country-list__item">
             <img src="${flags.svg}" alt="flag" width="50">
-            <p>${name.official}</p>
+            <p class="country-list__name bold">${name.official}</p>
         </li>`
-    ).join('')
+    ).join('');
     
-    refs.countryListRef.innerHTML = render
+    refs.countryListRef.innerHTML = render;
 }
 
 const onFetch = (event) => {
@@ -70,25 +74,30 @@ const onFetch = (event) => {
     const countryFromInput = event.target.value.trim();
     console.log(countryFromInput);
 
+
     fetchCountries(countryFromInput)
     .then((countryList) => {
-        console.log(countryList)
+        console.log(countryList);
         if (countryList.length > 10) {
             Notify.info('Too many matches found. Please enter a more specific name');
-        }
+        } 
 
         render(countryList);
     }
     )
     .catch((error) => {
-        console.log(error),
-    Notify.failure(`Oops, there is no country with ${countryFromInput} name`)
+        console.log(error);
+            if (countryFromInput === "") {
+                console.log("clear string");
+                refs.countryInfoRef.innerHTML = "";
+                refs.countryListRef.innerHTML = "";
+            Notify.info('Select a country, please');
+        } else {Notify.failure(`Oops, there is no country with ${countryFromInput} name`)}
     }
     );
-
 };
 
-refs.inputRef.addEventListener("input", onFetch)
+refs.inputRef.addEventListener("input", (debounce(onFetch, DEBOUNCE_DELAY)))
 
 
 
